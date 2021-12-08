9f57b15f96ca01a8a303313261fc618a8a13e923
Jay Miller <kjaymiller@gmail.com>
2021-12-08 13:51:44 -0800

    update tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: {
+    enabled: true,
+    mode: 'all',
+    content: [
+      './output/**/*.html',
+      './output/**/*.js',
+    ],
+  },
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+  ],
+}
